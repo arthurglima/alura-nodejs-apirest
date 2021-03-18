84697140b1eb117398838a4c12974fb3986f6b24
Arthur Lima <arthur_lima_1@hotmail.com>
2021-03-18 10:26:10 -0300

    Aula 03 - recebendo e salvando dados no banco de dados

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const consign = require('consign')
-
-
+ 
 module.exports = () => {
-    const app = express()
-
-    app.use(express.urlencoded({extended: true}))
-    app.use(express.json())
-
-    consign()
-        .include('controllers')
-        .into(app)
-    
-    return app
+ const app = express()
+ 
+ app.use(express.json())
+ app.use(express.urlencoded({ extended: true }))
+ 
+ consign()
+   .include('controllers')
+   .into(app)
+ 
+ return app
 }
